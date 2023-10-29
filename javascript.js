bf393bb93e312669347b4c66556e85d963d6055f
juiceman1014 <justintran1014@gmail.com>
2023-10-29 13:49:32 -0700

    async/await implemented

---
+++ javascript.js
@@ -0,0 +1,9 @@
+const img = document.querySelector("img");
+
+async function getCats(){
+  const response = await fetch("https://api.giphy.com/v1/gifs/translate?api_key=GYfcaPsJBQrP20VZPxqMRAY38fRd9eBP&s=cats", {mode: "cors"});
+  const catData = await response.json();
+  img.src = catData.data.images.original.url;
+}
+
+getCats();
